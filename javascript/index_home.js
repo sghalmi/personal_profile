a8b8c54017c484197379c75fc37a140e0878874f
sghalmi <sghalmi@umich.edu>
2024-04-14 16:35:07 -0400

    maybe done

---
+++ javascript/index_home.js
@@ -0,0 +1,36 @@
+// controls the dark and light mode 
+document.querySelector(".dark_ness").addEventListener("click", function () {
+    if (document.querySelector(".dark_mode").style.display == "block") {
+        document.querySelector(".dark_mode").style.display = "none";
+        document.querySelector(".light_mode").style.display = "block";
+        document.querySelector("body").style.backgroundColor = "white";
+        document.querySelector("body").style.color = "black";
+        document.querySelector(".align_together_nav").style.backgroundColor = "white";
+        document.querySelector(".nav_tab").style.backgroundColor = "white";
+        document.getElementsByClassName("nav_color")[0].style.color = "black";
+        document.getElementsByClassName("nav_color")[1].style.color = "black";
+        document.getElementsByClassName("nav_color")[2].style.color = "black";
+        document.querySelector(".active").style.color = "black";
+
+    }
+    else {
+        document.querySelector(".dark_mode").style.display = "block";
+        document.querySelector(".light_mode").style.display = "none";
+        document.querySelector("body").style.backgroundColor = "black";
+        document.querySelector("body").style.color = "white";
+        document.querySelector(".align_together_nav").style.backgroundColor = "black";
+        document.querySelector(".nav_tab").style.backgroundColor = "black";
+        document.getElementsByClassName("nav_color")[0].style.color = "white";
+        document.getElementsByClassName("nav_color")[1].style.color = "white";
+        document.getElementsByClassName("nav_color")[2].style.color = "white";
+        document.querySelector(".active").style.color = "white";
+    }
+});
+
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+}
---
+++ javascript/proj.js
@@ -0,0 +1,32 @@
+window.addEventListener('scroll', function(e) {    
+    if (window.scrollY >200 && window.innerWidth >= 720) {
+        console.log("hiii");
+        document.querySelector('.side_bar').setAttribute('id','past_pt_sidebar'); 
+        document.querySelector('.besides_sidebar').setAttribute('id','past_besides');  
+        document.querySelector('.information').setAttribute('id','pls_work');     
+    }
+    
+    if (window.scrollY < 200  && window.innerWidth >= 720) {
+        console.log("goooo");
+        document.querySelector('.side_bar').removeAttribute('id','past_pt_sidebar'); 
+        document.querySelector('.besides_sidebar').removeAttribute('id','past_besides');  
+        document.querySelector('.information').removeAttribute('id','pls_work');             
+
+    }
+});
+
+window.addEventListener('scroll', function(e) {    
+    if (window.scrollY >200 && window.innerWidth < 720) {
+        console.log("hiii");
+        document.querySelector('.side_bar').setAttribute('id','mobile_side_bar'); 
+
+
+    }
+    
+    if (window.scrollY < 200  && window.innerWidth < 720) {
+        console.log("goooo");
+        document.querySelector('.side_bar').removeAttribute('id','mobile_side_bar'); 
+    }
+});
+  
+  
