4b349cdb720f1897e272befdda6528491c3ebf3f
sghalmi <sghalmi@umich.edu>
2024-04-17 20:08:01 -0400

    almost done

---
+++ javascript/fashion.js
@@ -0,0 +1,18 @@
+let list_img = document.querySelectorAll('.front');
+let back_desc = document.querySelectorAll('.back');
+
+var clicked_i = false;
+var clicked_b = false;
+list_img.forEach(function (i) {
+    i.addEventListener('click', function () {
+        i.style.display = "none";
+        i.nextElementSibling.style.display = 'block';
+    });
+});
+
+back_desc.forEach(function (j) {
+    j.addEventListener('click', function () {
+        j.style.display = "none";
+        j.previousElementSibling.style.display = 'block';
+    });
+});
--- javascript/proj.js
+++ javascript/proj.js
@@ -1,12 +1,12 @@
 window.addEventListener('scroll', function(e) {    
-    if (window.scrollY >200 && window.innerWidth >= 720) {
+    if (window.scrollY >725 && window.innerWidth >= 720) {
         console.log("hiii");
         document.querySelector('.side_bar').setAttribute('id','past_pt_sidebar'); 
         document.querySelector('.besides_sidebar').setAttribute('id','past_besides');  
         document.querySelector('.information').setAttribute('id','pls_work');     
     }
     
-    if (window.scrollY < 200  && window.innerWidth >= 720) {
+    if (window.scrollY < 725  && window.innerWidth >= 720) {
         console.log("goooo");
         document.querySelector('.side_bar').removeAttribute('id','past_pt_sidebar'); 
         document.querySelector('.besides_sidebar').removeAttribute('id','past_besides');  
